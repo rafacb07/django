a0c8da51d22497d8073d257294191c55f43540ca
Rafa Crespo <rcrespo@linkedin.com>
2015-01-04 21:29:47 -0800

    added raw query for timesheet data

--- apps/papi/timesheet/views/views.py
+++ apps/papi/timesheet/views/views.py
@@ -8,7 +8,7 @@ from timesheet.models import Employee, Employer, Timesheet
 def index(request):
     employees  = Employee.objects.order_by('id')
     employers  = Employer.objects.order_by('id')
-    timesheets = Timesheet.objects.order_by('id')
+    timesheets = Timesheet.objects.raw('Select t.id, t.report_date, e.name from timesheets as t, employees as e where t.employee_id=e.id order by id asc')
 
     context = {
     	'employees': employees,
